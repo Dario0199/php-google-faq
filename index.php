9c0e906efd1365c783f66ff2e50100e3c51fef33
Dario0122 <dario.di.cuia@gmail.com>
2021-12-20 15:43:50 +0100

    header

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
+    <link href="https: //fonts.googleapis.com/css2? famiglia= Lato:wght@300;400;500;600 &family= Montserrat:wght@400;700 & display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div class="top">
+            <img class="logo" src="./image/google.png" alt="google-img">
+            <span>Privacy & Termini</span>
+        </div>
+        <div class="bottom">
+            <ul>
+                <li>
+                    <a href="">
+                        Introduzione
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        Norme sulla privacy
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        Termini di servizio
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        Tecnologie
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        Domande frequenti
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+    </header>
+    
+</body>
+</html>
